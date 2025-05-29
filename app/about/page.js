360c09146bf9d8f6099827d32cb23190b34bd688
adarshadv056 <adarshadv056@gmail.com>
2025-05-29 06:41:19 +0530

    initialized git reop

---
+++ app/about/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <>
+      <div>About Docuzy</div>
+      <h1>
+        Docuzy is a platform that allows you to edit any type of document with ease.
+        It is powered by AI to enhance your editing experience.
+      </h1>
+    </>
+  )
+}
+
+export default about
